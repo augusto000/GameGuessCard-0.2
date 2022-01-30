82fd718cefdef0e443a442d2d5c6adc0052d70bb
Augusto Achá <40480509+augusto000@users.noreply.github.com>
2022-01-29 21:12:36 -0300

    Code added to cards_table file

--- game/cards_table.py
+++ game/cards_table.py
@@ -1,1 +1,14 @@
+from game.cards import Cards
 
+
+class Cards_table:    
+
+    def __init__(self):
+        self.is_playing=True
+        
+        #startgame module Initialize the game.
+        while self.is_playing:
+                                            
+            #card.player_choice look for the choice of the player.
+            card = Cards()    
+            card.recursion()
